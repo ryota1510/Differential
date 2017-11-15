5c285a794eec8c976001fb25fc3b4437de5b458f
50616003 <50616003@50616003-pc>
2017-11-15 11:09:58 +0900

    a = 1 とし、hの値を1/10ずつ変化させていった時の 相対誤差の変化をチェックしていった h	error 0.1	5 0.01	0.5 0.001	0.05 0.0001	0.005 0.00001	5.00E-04 0.000001	5.00E-05 0.0000001	5.05E-06 0.00000001	6.08E-07 0.000000001	8.27E-06
    この結果から、0.0000001(1*10^(-7))と
    0.000000001(1*10^(-9))の間の
    0.00000001(1*10^(-8))で相対誤差が
    最小になっていることが読み取れる。
    1*10^(-8)付近で相対誤差が最小となることが
    分かったので、h = 0.9*10^(-8),1.1*10^(-8)
    での相対誤差を求めた
    0.000000009	8.73E-07
    0.000000011	1.21E-06
    すると、いずれもh = 1*10^(-8)のときの相対誤差
    6.08E-07よりも大きくなってしまったので、精度が一番
    良いときのhの値はh = 1*10^(-8)のときでその時の
    相対誤差は6.08E-07であると予想した。

---
+++ src/differential/Differential_main.java
@@ -0,0 +1,20 @@
+package differential;
+
+import java.util.Scanner;
+
+public class Differential_main {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input value of a> ");
+		double a = Double.parseDouble(scan.next());
+		System.out.print("Input value of h> ");
+		double h = Double.parseDouble(scan.next());
+		
+		Differential_lib dlib = new Differential_lib(a, h);
+		System.out.println("f(x)=x^2‚Ìx="+a+"‚É‚¨‚¯‚é”÷•ªŒW”‚Í"+dlib.getRx());
+		System.out.println("f(x)=x^2‚Ìx="+a+"‚É‚¨‚¯‚é”÷•ªŒW”‚Í"+dlib.getDx());
+		System.out.println("f(x)=x^2‚Ìx="+a+"‚É‚¨‚¯‚é”÷•ªŒW”‚Ì‘Š‘ÎŒë·‚Í"+dlib.error());
+	}
+
+}
